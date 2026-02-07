234b89958044761fdbe40b57e61be02459436fa1
annagianna <anna.giannakogeorgou@gmai.com>
2026-02-07 19:06:09 +0100

    added fasting insulin

--- scripts/1_tableone.R
+++ scripts/1_tableone.R
@@ -6,7 +6,7 @@ library(tableone)
 library(tidyverse)
 
 # Data
-baria_muscle_clean <- readRDS("data/260203_BARIA_muscle_clinical.RDS")
+baria_muscle_clean <- readRDS("data/260206_BARIA_muscle_clinical.RDS")
 
 # Table 1 grouped by %ASM change at 1y group
 t1_1y <- baria_muscle_clean |> 
@@ -17,14 +17,14 @@ t1_1y <- baria_muscle_clean |>
     t2d_v0 = fct_relevel(t2d_v0, "no")
   ) |>
   CreateTableOne(
-    vars = c("age_v0", "sex", "bmi_v0", "wc_cm_v0", "fm_kg_v0", "ffm_kg_v0", "asm_kg_v0", "t2d_v0", "hba1c_percent_v0", "fasting_glucose_mmoll_mmt_v0", "homa_ir_v0"),
+    vars = c("age_v0", "sex", "bmi_v0", "wc_cm_v0", "fm_kg_v0", "ffm_kg_v0", "asm_kg_v0", "t2d_v0", "hba1c_percent_v0", "fasting_glucose_mmoll_mmt_v0", "fasting_insulin_pmoll_mmt_v0", "homa_ir_v0"),
     factorVars = c("sex", "t2d_v0"),
     strata = "asm_change_v4_group",
     test = TRUE,
     addOverall = TRUE
   ) |> 
   print(
-    nonnormal = c("bmi_v0", "wc_cm_v0", "fm_kg_v0", "ffm_kg_v0", "asm_kg_v0", "t2d_v0", "hba1c_percent_v0", "fasting_glucose_mmoll_mmt_v0", "homa_ir_v0"),
+    nonnormal = c("age_v0", "bmi_v0", "wc_cm_v0", "fm_kg_v0", "ffm_kg_v0", "asm_kg_v0", "t2d_v0", "hba1c_percent_v0", "fasting_glucose_mmoll_mmt_v0", "fasting_insulin_pmoll_mmt_v0", "homa_ir_v0"),
     showAllLevels = FALSE,
     noSpaces = TRUE,
     pDigits = 3,
@@ -42,7 +42,22 @@ t1_1y <- baria_muscle_clean |>
   ) |> 
   tibble::rownames_to_column(var = "Variables") |> 
   mutate(
-    Variables = c("n", "Age (years)", "Sex", "BMI (kg/m²)", "WC (cm)", "FM (kg)", "FFM (kg)", "ASM (kg)", "T2D", "HbA1c (%)", "FPG (mmol/L)", "HOMA-IR")
+    Variables = recode(
+      Variables,
+      "n" = "n",
+      "age_v0" = "Age (years)",
+      "sex" = "Sex",
+      "bmi_v0" = "BMI (kg/m²)",
+      "wc_cm_v0" = "WC (cm)",
+      "fm_kg_v0" = "FM (kg)",
+      "ffm_kg_v0" = "FFM (kg)",
+      "asm_kg_v0" = "ASM (kg)",
+      "t2d_v0" = "T2D",
+      "hba1c_percent_v0" = "HbA1c (%)",
+      "fasting_glucose_mmoll_mmt_v0" = "FPG (mmol/L)",
+      "fasting_insulin_pmoll_mmt_v0" = "Fasting insulin (pmol/L)",
+      "homa_ir_v0" = "HOMA-IR"
+    )
   )
 
 # write table
