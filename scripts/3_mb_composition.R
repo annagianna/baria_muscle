8557cf47693b4fcdabd782410f604becd7f8a36b
annagianna <anna.giannakogeorgou@gmail.com>
2026-06-29 18:55:30 +0200

    typo

--- scripts/3_mb_composition.R
+++ scripts/3_mb_composition.R
@@ -72,6 +72,7 @@ theme_Publication <- function(base_size=14, base_family="sans") {
 baria_muscle <- read_rds("data/20260624_BARIA_muscle_clinical.RDS") # metadata/clinical data CHECK FOR MOST RECENT VERSION
 baria_mb <- read_rds("data/ps.BARIA.metaphlan.706.2548.RDS")
 
+# Melt into df
 melted_mb <- psmelt(baria_mb)
 
 # QC
