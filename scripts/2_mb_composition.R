94dfaaf1530d07344172e9b7c8250ed3a6a41fb5
annagianna <anna.giannakogeorgou@gmail.com>
2026-07-31 09:09:54 +0200

    clean metadata file already excludes abtibiotic users

--- scripts/2_mb_composition.R
+++ scripts/2_mb_composition.R
@@ -39,12 +39,8 @@ theme_minimal_composition <- function(base_size = 14, base_family = "sans") {
 }
 
 # Data
-baria_muscle_ab <- read_rds("data/20260722_BARIA_muscle_clinical.RDS") # metadata/clinical data CHECK FOR MOST RECENT VERSION
+baria_muscle <- read_rds("data/20260731_BARIA_muscle_clinical.RDS") # metadata/clinical data CHECK FOR MOST RECENT VERSION
 baria_mb <- read_rds("data/ps.BARIA.metaphlan.706.2548.RDS")
-
-# Filter out participants taking antibiotics
-baria_muscle <- baria_muscle_ab |> 
-  filter(abx_v0 == "no")
 
 # Melt into df
 melted_mb <- psmelt(baria_mb)
