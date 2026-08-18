21ecd7fdfc1f3756a1c3942eec849d09aca16f39
annagianna <anna.giannakogeorgou@gmail.com>
2026-08-18 09:21:39 +0200

    updated mb data cleaning and removed duplicate cols; pushed processed data files to today's date

--- scripts/0a_datacleaning.R
+++ scripts/0a_datacleaning.R
@@ -489,6 +489,7 @@ baria_mb_clean <- prune_samples(sample_data(run1_mb)$id %in% bia_abx_mb_ids, run
 # Add relevant metadata to mb
 baria_mb_metadata <- as(sample_data(baria_mb_clean), "data.frame") |> 
   rownames_to_column(var = "Sample") |>
+  select(-any_of(c("sex", "ffmi_v0", "low_ffmi_v0"))) |>
   left_join(
     baria_muscle_wide |> 
       select(id, sex, ffmi_v0, low_ffmi_v0),
@@ -500,13 +501,12 @@ sample_data(baria_mb_clean) <- sample_data(baria_mb_metadata)
 
 # Save clinical data as both RDS and csv files
 # Long clinical data
-write.csv(baria_muscle_long, "data/processed_data/260817_BARIA_muscle_long.csv")
-saveRDS(baria_muscle_long, "data/processed_data/260817_BARIA_muscle_long.RDS")
+write.csv(baria_muscle_long, "data/processed_data/260818_BARIA_muscle_long.csv")
+saveRDS(baria_muscle_long, "data/processed_data/260818_BARIA_muscle_long.RDS")
 
 # Wide clinical data
-write.csv(baria_muscle_wide, "data/processed_data/260817_BARIA_muscle_wide.csv")
-saveRDS(baria_muscle_wide, "data/processed_data/260817_BARIA_muscle_wide.RDS")
+write.csv(baria_muscle_wide, "data/processed_data/260818_BARIA_muscle_wide.csv")
+saveRDS(baria_muscle_wide, "data/processed_data/260818_BARIA_muscle_wide.RDS")
 
 # Save mb data
-saveRDS(baria_mb_clean, "data/processed_data/260817_BARIA_mb_clean.RDS")
-
+saveRDS(baria_mb_clean, "data/processed_data/260818_BARIA_mb_clean.RDS")
