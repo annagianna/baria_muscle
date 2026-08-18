e5490018ab164cde2de5f8cfb818f4c041fd7999
annagianna <anna.giannakogeorgou@gmail.com>
2026-08-18 22:38:55 +0200

    tableone, alpha & beta diversity, baseline mb comp plot stratified by %FFMI loss group at 1y

---
+++ scripts/2f_mb_composition_ffmi_1y.R
@@ -0,0 +1,116 @@
+# Baseline microbial composition stratified by 1-year FFMI loss group
+# Anna Giannakogeorgou, a.gianna@amsterdamumc.nl
+
+# Packages
+library(tidyverse)
+library(phyloseq)
+library(grid)
+library(MetBrewer)
+
+# Theme
+theme_minimal_composition <- function(base_size = 14, base_family = "sans") {
+
+  theme_minimal(base_size = base_size, base_family = base_family) +
+    theme(
+      plot.title = element_text(face = "bold", size = rel(1), hjust = 0.5),
+      axis.title = element_text(face = "bold", size = rel(1)),
+      axis.title.y = element_text(angle = 90, vjust = 2),
+      axis.text = element_text(colour = "black"),
+      axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+      axis.line.x.bottom = element_line(colour = "black", linewidth = 0.5),
+      axis.line.y.left = element_line(colour = "black", linewidth = 0.5),
+      axis.ticks = element_line(colour = "black", linewidth = 0.4),
+      panel.grid.major.y = element_line(colour = "#dddddd", linewidth = 0.4, linetype = "22"),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(fill = "white", colour = NA),
+      plot.background = element_rect(fill = "white", colour = NA),
+      legend.position = "right",
+      legend.key.height = unit(0.4, "cm"),
+      legend.key.width = unit(0.4, "cm"),
+      legend.spacing.y = unit(0, "cm"),
+      legend.text = element_text(size = rel(0.7)),
+      plot.margin = unit(c(10, 5, 5, 5), "mm")
+    )
+}
+renoir_20 <- met.brewer("Renoir", n = 20)
+
+# Data
+baria_mb <- readRDS("data/processed_data/260818_BARIA_mb_clean.RDS") # requires perc_change_ffmi_v4_group in metadata
+
+# Melt into df
+melted_mb <- psmelt(baria_mb)
+
+#### Composition Plots stratified by 1y FFMI loss group ###
+### Species level ###
+# Summarize per group and identify top 20 species at baseline
+top20_species_v0 <- melted_mb |> 
+  filter(visit == "v0") |>
+  group_by(Sample, Species) |> 
+  summarize(Abundance = sum(Abundance), .groups = "drop") |> 
+  group_by(Species) |> 
+  summarize(Abundance = mean(Abundance)) |> 
+  arrange(-Abundance) |> 
+  slice_head(n = 20)
+
+### Baseline composition ###
+species_ffmi_loss_v0 <- melted_mb |>
+  filter(visit == "v0", !is.na(perc_change_ffmi_v4_group)) |>
+  mutate(
+    Species2 = if_else(
+      Species %in% top20_species_v0$Species,
+      Species,
+      "Other species" # Collapse other species
+    )
+  ) |>
+  group_by(Species2, Sample, perc_change_ffmi_v4_group) |>
+  summarize(Abundance = sum(Abundance), .groups = "drop") |>
+  group_by(Species2, perc_change_ffmi_v4_group) |> # species per FFMI loss group
+  summarize(Abundance = mean(Abundance), .groups = "drop") # avg abundance per species per group
+
+# check sum
+species_ffmi_loss_v0 |> # check
+  group_by(perc_change_ffmi_v4_group) |> 
+  summarize(sum_Abundance = sum(Abundance)) # adds up to 100
+
+# Create order based on the abundance in the high-loss group
+species_order_high_ffmi_loss_v0 <- species_ffmi_loss_v0 |> 
+  filter(
+    perc_change_ffmi_v4_group == "high",
+    Species2 != "Other species"
+  ) |> 
+  arrange(-Abundance) |> 
+  pull(Species2)
+
+species_ffmi_loss_v0 <- species_ffmi_loss_v0 |>
+  mutate(Species2 = factor(Species2, levels = c(species_order_high_ffmi_loss_v0, "Other species")))
+  
+# Assign colors according to abundance order in the high-loss group
+set.seed(13)
+top20_species_v0_high_colours <- c("Other species" = "grey75", setNames(sample(renoir_20), species_order_high_ffmi_loss_v0))
+
+# Composition plot
+species_comp_ffmi_loss_v0 <- species_ffmi_loss_v0 |> 
+  mutate(
+    perc_change_ffmi_v4_group = fct_relevel(perc_change_ffmi_v4_group, "high", after = 0L) # high loss first
+  ) |> 
+  ggplot(aes(x = perc_change_ffmi_v4_group, y = Abundance, fill = Species2)) +
+  geom_bar(
+    stat = "identity",
+    color = "black",
+    width = 0.65,
+    position = position_stack(reverse = TRUE)
+  ) +
+  scale_fill_manual(values = top20_species_v0_high_colours) +
+  guides(fill = guide_legend(ncol = 1, reverse = TRUE)) +
+  labs(x = "FFMI loss group at 1 year", y = "Relative abundance (%)", title = "Species", fill = "") +
+  scale_x_discrete(labels = c("high" = "High", "modest/low" = "Modest/low")) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_minimal_composition() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+    legend.text = element_text(size = rel(0.9))
+  )
+ggsave(plot = species_comp_ffmi_loss_v0, "graphs/composition/species_comp_by_ffmi_loss_v4.pdf", width = 14, height = 8)
+
