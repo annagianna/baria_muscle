469463952cc38988d6a3e486a5620ffb67f774c4
annagianna <anna.giannakogeorgou@gmail.com>
2026-08-25 18:20:11 +0200

    updated source file name & var names

--- scripts/1a_tableone_v0.R
+++ scripts/1a_tableone_v0.R
@@ -6,13 +6,15 @@ library(tidyverse)
 library(tableone)
 
 # Data
-baria_muscle_wide <- readRDS("data/processed_data/260818_BARIA_muscle_wide.RDS")
+baria_muscle_wide <- readRDS("data/processed_data/BARIA_muscle_wide.RDS")
 
 #### Table 1 grouped by FFMI status at baseline (participants with available shotgun data) ####
 # Choose vars
 t1_v0_mb_vars <- baria_muscle_wide |> 
-  select(age_v0, sex, bmi_v0, wc_cm_v0, fm_kg_v0, ffm_kg_v0, ffmi_v0, low_ffmi_v0, prediab_labs_v0, t2d_v0, 
-    hba1c_mmolmol_v0, fasting_glucose_mmoll_mmt_v0, fasting_insulin_pmoll_mmt_v0, homa_ir_v0, homa_b_v0) |> 
+  select(
+    age_v0, sex, bmi_v0, wc_cm_v0, fm_kg_v0, ffm_kg_v0, ffmi_v0, low_ffmi_v0, prediab_labs_v0, t2d_v0, 
+    hba1c_mmolmol_v0, glucose_mmoll_mmt_0_v0, insulin_pmoll_mmt_0_v0, homa_ir_v0, homa_b_v0
+  ) |> 
   filter(!is.na(low_ffmi_v0)) |> 
   mutate(
     low_ffmi_v0 = factor(
@@ -33,8 +35,8 @@ t1_v0_mb_vars <- baria_muscle_wide |>
     `Prediabetes` = prediab_labs_v0,
     `T2D` = t2d_v0,
     `HbA1c (mmol/mol)` = hba1c_mmolmol_v0,
-    `Fasting glucose (mmol/L)` = fasting_glucose_mmoll_mmt_v0,
-    `Fasting insulin (pmol/L)` = fasting_insulin_pmoll_mmt_v0,
+    `Fasting glucose (mmol/L)` = glucose_mmoll_mmt_0_v0,
+    `Fasting insulin (pmol/L)` = insulin_pmoll_mmt_0_v0,
     `HOMA-IR` = homa_ir_v0,
     `HOMA-B` = homa_b_v0
   )
